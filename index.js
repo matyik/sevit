2a6a6ee9f9c6941f8f0135901667fdcbc16de404
Matyi Kari <matykari@gmail.com>
2022-01-07 16:49:36 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+import express from 'express'
+import Web3 from 'web3'
+const web3 = new Web3(
+  Web3.givenProvider ||
+    'https://eth-mainnet.alchemyapi.io/v2/U3mc_zqiTxo1w0KKpTgWsKfyEh1XO7aK'
+)
+const app = express()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
+
+app.use(express.json())
+
+app.get(`/contract/:method`, async (req, res) => {
+  const ABI = req.body
+  const { method } = req.params
+  const { contractAddress } = req.query
+  try {
+    const contract = new web3.eth.Contract(ABI, contractAddress)
+    const result = await contract.methods[method](
+      ...Object.entries(req.query)
+        .filter((query) => query[0] !== 'contractAddress')
+        .map((query) => query[1])
+    ).call()
+    res.json({ result })
+  } catch (err) {
+    if (err.message === 'contract.methods[method] is not a function') {
+      return res.status(400).json({ error: 'Method not found' })
+    }
+    res.status(500).json({ error: err.message })
+  }
+})
+
+app.get('/balance/:address', async (req, res) => {
+  const { address } = req.params
+  try {
+    const balance = await web3.eth.getBalance(address)
+    res.json({ balance })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+app.get('/ens/:name', async (req, res) => {
+  const { name } = req.params
+  try {
+    const address = await web3.eth.ens.getAddress(name)
+    res.json({ address })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+app.get('/gasprice', async (req, res) => {
+  try {
+    const gasPrice = await web3.eth.getGasPrice()
+    res.json({ gasPrice })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+app.get('/transaction/:transactionHash', async (req, res) => {
+  const { transactionHash } = req.params
+  try {
+    const transaction = await web3.eth.getTransaction(transactionHash)
+    res.json({ transaction })
+  } catch (error) {
+    if (
+      error.message.indexOf(
+        'Returned error: invalid argument 0: hex string has length'
+      ) !== -1 ||
+      error.message ===
+        'Returned error: invalid argument 0: json: cannot unmarshal hex string of odd length into Go value of type common.Hash'
+    ) {
+      return res.status(400).json({ error: 'Invalid transaction hash' })
+    }
+    res.status(500).json({ error: error.message })
+  }
+})
